4ea2095da0d6dc272095b540460d3db16987e21b
Andrew Bliss <andrewgbliss@gmail.com>
2025-11-17 21:25:38 -0700

    updates

--- blisscode/weather/weather.gd
+++ blisscode/weather/weather.gd
@@ -1,38 +1,63 @@
 class_name Weather extends Node2D
 
-@export var weather_service_type: WeatherService.WeatherType = WeatherService.WeatherType.NONE
+enum WeatherType {
+	RAIN,
+	SNOW,
+	HAIL,
+	WIND_TRAILS,
+	LEAVES,
+	FOG,
+}
+
+@export var weather_type: WeatherType = WeatherType.RAIN
 @export var particles: GPUParticles2D
 @export var light_material: ParticleProcessMaterial
 @export var heavy_material: ParticleProcessMaterial
+@export var gravity_x: float = 0.0
 
 func _ready() -> void:
 	stop()
 	WeatherService.weather_changed.connect(_on_weather_changed)
 
-func _on_weather_changed(weather_type: WeatherService.WeatherType, weather_intensity: WeatherService.WeatherIntensity, fog_intensity: WeatherService.FogIntensity, wind_direction: Vector2) -> void:
-	if weather_service_type == weather_type:
+func _get_weather_type() -> String:
+	match weather_type:
+		WeatherType.RAIN:
+			return "rain"
+		WeatherType.SNOW:
+			return "snow"
+		WeatherType.HAIL:
+			return "hail"
+		WeatherType.WIND_TRAILS:
+			return "wind_trails"
+		WeatherType.LEAVES:
+			return "leaves"
+		WeatherType.FOG:
+			return "fog"
+		_:
+			return ""
+
+func _on_weather_changed(weather: Dictionary[String, bool], weather_intensity: WeatherService.WeatherIntensity, wind_direction: Vector2) -> void:
+	if weather[_get_weather_type()]:
 		start()
 	else:
 		stop()
 
 	match wind_direction:
 		Vector2.LEFT:
-			light_material.direction.x = -1
-			heavy_material.direction.x = -1
+			light_material.gravity.x = - gravity_x
+			heavy_material.gravity.x = - gravity_x
 		Vector2.RIGHT:
-			light_material.direction.x = 1
-			heavy_material.direction.x = 1
+			light_material.gravity.x = gravity_x
+			heavy_material.gravity.x = gravity_x
 		Vector2.ZERO:
-			light_material.direction.x = 0
-			heavy_material.direction.x = 0
+			light_material.gravity.x = 0
+			heavy_material.gravity.x = 0
 
 	match weather_intensity:
 		WeatherService.WeatherIntensity.LIGHT:
 			particles.process_material = light_material
 		WeatherService.WeatherIntensity.HEAVY:
 			particles.process_material = heavy_material
-
-	print("fog_intensity: ", fog_intensity)
 	
 func start():
 	particles.emitting = true
--- blisscode/weather/weather_service.gd
+++ blisscode/weather/weather_service.gd
@@ -1,62 +1,50 @@
 extends Node
 
-enum WeatherType {
-	NONE,
-	RAIN,
-	SNOW,
-	HAIL,
-}
+@export var weather: Dictionary[String, bool] = {
+	"rain": false,
+	"snow": false,
+	"hail": false,
+	"wind_trails": false,
+	"leaves": false,
+	"fog": false,
+}:
+	set = set_weather, get = get_weather
+
+func set_weather(value: Dictionary[String, bool]) -> void:
+	weather = value
+	weather_changed.emit(weather, weather_intensity, wind_direction)
+
+func get_weather() -> Dictionary[String, bool]:
+	return weather
+
+func set_weather_key(key: String, value: bool) -> void:
+	weather[key] = value
+	weather_changed.emit(weather, weather_intensity, wind_direction)
+
+func get_weather_key(key: String) -> bool:
+	return weather[key]
 
 enum WeatherIntensity {
 	LIGHT,
 	HEAVY,
 }
 
-enum FogIntensity {
-	NONE,
-	LIGHT,
-	HEAVY,
-}
-
-@export var weather_type: WeatherType = WeatherType.NONE:
-	set = set_weather_type, get = get_weather_type
-func set_weather_type(value: WeatherType) -> void:
-	weather_type = value
-	weather_changed.emit(weather_type, weather_intensity, fog_intensity, wind_direction)
-	_update_debug_label()
-
-func get_weather_type() -> WeatherType:
-	return weather_type
-
 @export var weather_intensity: WeatherIntensity = WeatherIntensity.LIGHT:
 	set = set_weather_intensity, get = get_weather_intensity
 
 func set_weather_intensity(value: WeatherIntensity) -> void:
 	weather_intensity = value
-	weather_changed.emit(weather_type, weather_intensity, fog_intensity, wind_direction)
-	_update_debug_label()
+	weather_changed.emit(weather, weather_intensity, wind_direction)
 
 func get_weather_intensity() -> WeatherIntensity:
 	return weather_intensity
-
-@export var fog_intensity: FogIntensity = FogIntensity.NONE:
-	set = set_fog_intensity, get = get_fog_intensity
-	
-func set_fog_intensity(value: FogIntensity) -> void:
-	fog_intensity = value
-	weather_changed.emit(weather_type, weather_intensity, fog_intensity, wind_direction)
-	_update_debug_label()
-
-func get_fog_intensity() -> FogIntensity:
-	return fog_intensity
 
 @export var wind_direction: Vector2 = Vector2.LEFT:
 	set = set_wind_direction, get = get_wind_direction
 
 func set_wind_direction(value: Vector2) -> void:
 	wind_direction = value
-	weather_changed.emit(weather_type, weather_intensity, fog_intensity, wind_direction)
-	_update_debug_label()
+	weather_changed.emit(weather, weather_intensity, wind_direction)
 
 func get_wind_direction() -> Vector2:
 	return wind_direction
@@ -64,7 +52,7 @@ func get_wind_direction() -> Vector2:
 @export var debug_panel: Panel
 @export var debug_label: Label
 
-signal weather_changed(weather_type: WeatherType, weather_intensity: WeatherIntensity, fog_intensity: FogIntensity, wind_direction: Vector2)
+signal weather_changed(weather: Dictionary[String, bool], weather_intensity: WeatherIntensity, wind_direction: Vector2)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed and event.keycode == KEY_F1:
@@ -78,69 +66,31 @@ func _ready() -> void:
 	call_deferred("_after_ready")
 
 func _after_ready() -> void:
-	_update_debug_label()
-	weather_changed.emit(weather_type, weather_intensity, fog_intensity, wind_direction)
-
-func _update_debug_label():
-	if debug_label:
-		var weather_type_string = ""
-		match weather_type:
-			WeatherType.NONE:
-				weather_type_string = ""
-			WeatherType.RAIN:
-				weather_type_string = "Rain"
-			WeatherType.SNOW:
-				weather_type_string = "Snow"
-			WeatherType.HAIL:
-				weather_type_string = "Hail"
-		var weather_intensity_string = ""
-		match weather_intensity:
-			WeatherIntensity.LIGHT:
-				weather_intensity_string = "Breezy"
-			WeatherIntensity.HEAVY:
-				weather_intensity_string = "Windy"
-		var fog_intensity_string = ""
-		match fog_intensity:
-			FogIntensity.NONE:
-				fog_intensity_string = ""
-			FogIntensity.LIGHT:
-				fog_intensity_string = "Light Fog"
-			FogIntensity.HEAVY:
-				fog_intensity_string = "Heavy Fog"
-		var wind_direction_string = ""
-		match wind_direction:
-			Vector2.LEFT:
-				wind_direction_string = "Left Wind"
-			Vector2.RIGHT:
-				wind_direction_string = "Right Wind"
-		debug_label.text = "%s %s %s %s" % [weather_intensity_string, weather_type_string, fog_intensity_string, wind_direction_string]
+	weather_changed.emit(weather, weather_intensity, wind_direction)
 
 func _on_none_button_pressed() -> void:
-	set_weather_type(WeatherType.NONE)
+	set_weather({
+		"rain": false,
+		"snow": false,
+		"hail": false,
+		"wind_trails": false,
+		"leaves": false,
+	})
 
 func _on_rain_button_pressed() -> void:
-	set_weather_type(WeatherType.RAIN)
+	set_weather_key("rain", not get_weather_key("rain"))
 
 func _on_snow_button_pressed() -> void:
-	set_weather_type(WeatherType.SNOW)
+	set_weather_key("snow", not get_weather_key("snow"))
 
 func _on_hail_button_pressed() -> void:
-	set_weather_type(WeatherType.HAIL)
+	set_weather_key("hail", not get_weather_key("hail"))
 
 func _on_breezy_button_pressed() -> void:
 	set_weather_intensity(WeatherIntensity.LIGHT)
 
 func _on_windy_button_pressed() -> void:
 	set_weather_intensity(WeatherIntensity.HEAVY)
-
-func _on_no_fog_button_pressed() -> void:
-	set_fog_intensity(FogIntensity.NONE)
-
-func _on_light_fog_button_pressed() -> void:
-	set_fog_intensity(FogIntensity.LIGHT)
-
-func _on_heavy_fog_button_pressed() -> void:
-	set_fog_intensity(FogIntensity.HEAVY)
 
 func _on_left_wind_button_pressed() -> void:
 	set_wind_direction(Vector2.LEFT)
@@ -150,3 +100,15 @@ func _on_right_wind_button_pressed() -> void:
 
 func _on_down_wind_button_pressed() -> void:
 	set_wind_direction(Vector2.ZERO)
+	
+func _on_wind_check_button_toggled(toggled_on: bool) -> void:
+	set_weather_key("wind_trails", toggled_on)
+
+func _on_fog_check_button_toggled(toggled_on: bool) -> void:
+	set_weather_key("fog", toggled_on)
+
+func _on_heavy_check_button_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		set_weather_intensity(WeatherIntensity.HEAVY)
+	else:
+		set_weather_intensity(WeatherIntensity.LIGHT)
